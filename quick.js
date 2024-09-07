ae41620e04718edc474e69b7123e7b8cab083c84
Archi <archiagrawal470@gmail.com>
2024-09-07 23:03:52 +0530

    Add files via upload

---
+++ quick.js
@@ -0,0 +1,71 @@
+
+async function partitionLomuto(ele, l, r){
+    console.log('In partitionLomuto()');
+    let i = l - 1;
+    // color pivot element
+    ele[r].style.background = 'orange';
+    for(let j = l; j <= r - 1; j++){
+        console.log('In partitionLomuto for j');
+        // color current element
+        ele[j].style.background = 'yellow';
+        // pauseChamp
+        await waitforme(delay);
+
+        if(parseInt(ele[j].style.height) < parseInt(ele[r].style.height)){
+            console.log('In partitionLomuto for j if');
+            i++;
+            swap(ele[i], ele[j]);
+            // color 
+            ele[i].style.background = 'blue';
+            // pauseChamp
+            await waitforme(delay);
+        }
+        else{
+            // color if not less than pivot
+            ele[j].style.background = 'blue';
+        }
+    }
+    i++; 
+    // pauseChamp
+    await waitforme(delay);
+    swap(ele[i], ele[r]); // pivot height one
+    console.log(`i = ${i}`, typeof(i));
+    // color
+    ele[r].style.background = 'orange';
+    ele[i].style.background = 'green';
+
+    // pauseChamp
+    await waitforme(delay);
+
+    return i;
+}
+
+async function quickSort(ele, l, r){
+    console.log('In quickSort()', `l=${l} r=${r}`, typeof(l), typeof(r));
+    if(l < r){
+        let pivot_index = await partitionLomuto(ele, l, r);
+        await quickSort(ele, l, pivot_index - 1);
+        await quickSort(ele, pivot_index + 1, r);
+    }
+    else{
+        if(l >= 0 && r >= 0 && l <ele.length && r <ele.length){
+            ele[r].style.background = 'green';
+            ele[l].style.background = 'green';
+        }
+    }
+}
+
+
+const quickSortbtn = document.querySelector(".quickSort");
+quickSortbtn.addEventListener('click', async function(){
+    let ele = document.querySelectorAll('.bar');
+    let l = 0;
+    let r = ele.length - 1;
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    await quickSort(ele, l, r);
+    enableSortingBtn();
+    enableSizeSlider();
+    enableNewArrayBtn();
+});
